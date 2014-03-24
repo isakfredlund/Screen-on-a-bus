76346de5379b216993c1d84c8a405a73133c0770
eliaaos <elias.lindblad@gmail.com>
2014-03-24 14:07:59 +0100

    testar igen

--- src/view/TestClass.java
+++ src/view/TestClass.java
@@ -3,6 +3,7 @@ import model.Line;
 import xmlparser.Xmlparse;
 
 public class TestClass {
+	// ändring...
 	private Line linje;
 	private static Xmlparse parser;
 	private static String url = "http://www.labs.skanetrafiken.se/v2.2/stationresults.asp?selPointFrKey=80003";
