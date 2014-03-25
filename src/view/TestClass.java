e9251934b690027f01df91fa481fe3698df662a1
eliaaos <elias.lindblad@gmail.com>
2014-03-25 10:29:53 +0100

    commit

--- src/view/TestClass.java
+++ src/view/TestClass.java
@@ -3,17 +3,15 @@ import model.Line;
 import xmlparser.Xmlparse;
 
 public class TestClass {
-	// ändring...
 	private Line linje;
 	private static Xmlparse parser;
 	private static String url = "http://www.labs.skanetrafiken.se/v2.2/stationresults.asp?selPointFrKey=80003";
 	public static void main(String[] args) {
 		parser = new Xmlparse(url);
 		parser.yolo();
-		// swag
 		
 		
-		//bla
+		
 	}
 
 }
